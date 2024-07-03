5229d58fa902b870efd125be999a1d500e7b5bb1
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-07-03 15:44:42 +0200

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-
+"""
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
 import requests
+import sys
+
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit to query.
-        
-    Returns:
-        int: The number of subscribers if the subreddit is valid, otherwise 0.
-    """
-    
-    # Setting the User-Agent to avoid 'Too Many Requests' errors
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    
-    # URL to get subreddit info
-    url = f'https://api.reddit.com/r/{subreddit}/about.json'
-    
-    try:
-        # Make the request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Parse the JSON response
-            data = response.json()
-            # Return the number of subscribers
-            return data['data']['subscribers']
-        else:
-            # If subreddit is invalid or request failed
-            return 0
-    except requests.RequestException:
-        # Handle any request exceptions
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://api.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']

90ccf79399c97d6ac89b98ce0360b994b8d8c981
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-07-03 16:05:52 +0200

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,6 +7,7 @@ the number of subscribers for a given subreddit.
 import requests
 import sys
 
+
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers for a given subreddit.
@@ -26,27 +27,26 @@ def number_of_subscribers(subreddit):
     }
 
     # Construct the URL to get subreddit info
-    url = "https://api.reddit.com/r/{}/about.json".format(subreddit)
-    
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
     try:
         # Make the request to the Reddit API
         res = requests.get(url, headers=headers, allow_redirects=False)
-        
+
         # If the response status code is not 200, return 0
         if res.status_code != 200:
             return 0
-        
+
         # Parse the JSON response
         data = res.json()
-        
+
         # Check if the 'data' and 'subscribers' keys are in the response
         if 'data' not in data or 'subscribers' not in data['data']:
             return 0
-        
+
         # Return the number of subscribers
         return data['data']['subscribers']
-    
+
     except requests.RequestException:
         # Handle any request exceptions and return 0
         return 0
-    

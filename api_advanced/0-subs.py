ffe0527f9217331282ca6af5a9c8d66547c76d0c
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-07-03 15:51:02 +0200

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,20 +8,41 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-    """ Queries to Reddit API """
+    """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
     u_agent = 'Mozilla/5.0'
 
+    # Set headers with custom User-Agent to avoid rate limiting
     headers = {
         'User-Agent': u_agent
     }
 
-    url = "https://api.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+    # Construct the URL to get subreddit info
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    
+    try:
+        # Make the request to the Reddit API
+        res = requests.get(url, headers=headers, allow_redirects=False)
+        
+        # If the response status is not 200 (OK), return 0
+        if res.status_code != 200:
+            return 0
+        
+        # Parse the JSON response
+        data = res.json()
+        
+        # Check if the expected keys are in the response
+        if 'data' not in data or 'subscribers' not in data['data']:
+            return 0
+        
+        # Return the number of subscribers
+        return data['data']['subscribers']
+    
+    except requests.RequestException:
+        # Handle any request exceptions and return 0
         return 0
-    dic = res.json()
-    if 'data' not in dic:
-        return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
+
+# Example usage
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        subreddit = sys.argv[1]
+        print(f'Number of subscribers in r/{subreddit}: {number_of_subscribers(subreddit)}')
